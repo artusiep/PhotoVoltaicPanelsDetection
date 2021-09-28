4bec8c544d32c4a43b4419502cac83de25472184
andrzej.maj <andrzejmaj123@gmail.com>
2021-09-28 18:44:48 +0200

    Adjust augmentation script to recent path changes; add `augmentation_id` argument

--- neural/augmentation/data_augmentation.py
+++ neural/augmentation/data_augmentation.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 
 import Augmentor
@@ -16,12 +17,19 @@ debug = False
 count = 200  # num of images to generate
 batch = 20  # size of a single batch
 begin = 0  # indicate the current index, use only if you are continuing a crashed generation
-input_image_dir = '../data'
-input_mask_dir = '../masks'
+input_image_dir = '../data/input'
+input_ground_truth_dir = '../data/ground_truth'
 extension = '*.png'
+
 current_time = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
-out_image_dir = f'augmentation/{current_time}/image'
-out_mask_dir = f'augmentation/{current_time}/mask'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--augmentation_id', type=str, nargs='?', default=current_time)
+args = parser.parse_args()
+augmentation_id = args.augmentation_id
+
+out_image_dir = f'data/{augmentation_id}/input/'
+out_ground_truth_dir = f'data/{augmentation_id}/ground_truth/'
 
 
 def build_augmentation_pipeline(images):
@@ -41,20 +49,20 @@ def build_augmentation_pipeline(images):
     return augmentation_pipeline
 
 
-def read_images_and_masks():
-    # import images and corresponding masks in natural order
+def read_images_and_ground_truth():
+    # import images and corresponding ground truth images in natural order
     images = natsorted(glob.glob(os.path.join(input_image_dir, extension)))
 
-    mask_path = os.path.join(input_mask_dir, extension)
-    masks = natsorted(glob.glob(mask_path))
-    print(input_mask_dir + ":" + str(len(masks)))
+    ground_truth_path = os.path.join(input_ground_truth_dir, extension)
+    ground_truth_images = natsorted(glob.glob(ground_truth_path))
+    print(input_ground_truth_dir + ":" + str(len(ground_truth_images)))
 
-    return [[np.asarray(Image.open(y)) for y in x] for x in list(zip(images, masks))]
+    return [[np.asarray(Image.open(y)) for y in x] for x in list(zip(images, ground_truth_images))]
 
 
 def generate_augmented_data(augmentation_pipeline):
     Path(out_image_dir).mkdir(parents=True, exist_ok=True)
-    Path(out_mask_dir).mkdir(parents=True, exist_ok=True)
+    Path(out_ground_truth_dir).mkdir(parents=True, exist_ok=True)
 
     # begin generation
     for i in range(begin // batch, count // batch):
@@ -70,26 +78,26 @@ def generate_augmented_data(augmentation_pipeline):
             plt.show()
 
         for j in range(batch):
-            # save image and its corresponding mask
-            file_name = str(i * batch + j) + ".png"
+            # save image and its corresponding ground truth image
+            file_name = str(i * batch + j) + "_" + augmentation_id + ".png"
 
             # image
             out_image_path = os.path.join(out_image_dir, file_name)
             cv2.imwrite(out_image_path, augmented_images[j][0])
 
-            # mask
-            out_mask_path = os.path.join(out_mask_dir, file_name)
-            augmented_mask = augmented_images[j][1]
-            augmented_mask = cv2.cvtColor(augmented_mask, cv2.COLOR_BGR2GRAY)
+            # ground truth
+            out_ground_truth_path = os.path.join(out_ground_truth_dir, file_name)
+            augmented_ground_truth = augmented_images[j][1]
+            augmented_ground_truth = cv2.cvtColor(augmented_ground_truth, cv2.COLOR_BGR2GRAY)
 
-            thresh = np.mean(augmented_mask)
-            _, augmented_mask = cv2.threshold(augmented_mask, thresh, 255, cv2.THRESH_BINARY)
-            cv2.imwrite(out_mask_path, augmented_mask)
+            thresh = np.mean(augmented_ground_truth)
+            _, augmented_ground_truth = cv2.threshold(augmented_ground_truth, thresh, 255, cv2.THRESH_BINARY)
+            cv2.imwrite(out_ground_truth_path, augmented_ground_truth)
 
             print(str(i * batch + j) + " st image success")
             print(str(i) + " st batch finish")
 
 
-images_and_masks = read_images_and_masks()
-pipeline = build_augmentation_pipeline(images_and_masks)
+images_and_ground_truth = read_images_and_ground_truth()
+pipeline = build_augmentation_pipeline(images_and_ground_truth)
 generate_augmented_data(pipeline)
